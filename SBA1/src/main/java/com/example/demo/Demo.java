7c6af9be69e3f50ac00aa52d4754c86db4fd37f6
Ashutosh Nayak <ashutoshnayak.official@gmail.com>
2025-02-12 15:17:51 +0530

    day 10

---
+++ SBA1/src/main/java/com/example/demo/Demo.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employees")
+
+public class Demo {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is test api");
+
+	}
+
+}
